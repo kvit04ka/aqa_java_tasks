73307ea280e8100738882f9d75d42d72c47a34a0
kvit04ka <nataliaoziranska@gmail.com>
2020-02-03 12:00:35 +0200

    Update Task1.java

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -1,11 +1,9 @@
 package lesson1;
 
 public class Task1 {
-    /* Минимум трех чисел
-    Написать функцию, которая вычисляет минимум из трёх чисел.
-    */
+   
     public static void main(String[] args) {
-        //System.out.println(min(5,2,3));
+      
         System.out.println(min(5,2,3));
     }
 
