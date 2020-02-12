bd8bf99c232fadb26d641efa1ee8e61114c0634c
Natalia <nataozira@gmail.com>
2020-02-12 16:00:05 +0200

    Home task Done

--- src/main/java/lesson1/Task2.java
+++ src/main/java/lesson1/Task2.java
@@ -1,11 +1,5 @@
 package lesson1;
 
-/* Мое имя
-Вывести на экран свое имя 5 строк по 10 раз (через пробел).
-System.out.println, System.out.print можно использовать только по одному разу
-для этого нужно воспользоваться циклом for
-*/
-
 public class Task2 {
 
     public static void main(String[] args) {
--- src/main/java/lesson1/Task5.java
+++ src/main/java/lesson1/Task5.java
@@ -1,13 +1,8 @@
 
 package lesson1;
 
-/**
- * Реализовать функцию, которая принимает строку и возвращает ее же в обратном виде
- * например "Hello world!!!" -> "!!!dlrow olleH"
- */
 public class Task5 {
     public static void main(String[] args) {
-        //System.out.println(reverse("Hello world!!!"));
         String str = "Hello world!!!";
         String reverse = new StringBuffer(str).reverse().toString();
         System.out.println(reverse);
