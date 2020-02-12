ff9e899887fef96c8e90d49245e017badd11871c
Natalia <nataozira@gmail.com>
2020-02-12 15:53:31 +0200

    Home task Done

--- src/main/java/lesson2/Task1.java
+++ src/main/java/lesson2/Task1.java
@@ -1,24 +1,68 @@
 package lesson2;
 
-/**
- * Реализовать у класса Task1 переменные age (тип int),  name (тип String), ageGroup(тип String)
- * реализовать методы геттеры и сеттеры для каждой переменной.
- * Должны быть выполненны следующие условия:
- *  - не должно быть возможности напрямую устанваливать или читать значение age/name
- *  - нельзя установить значение age меньше нуля
- *  - максимальныое значение для возраста - 100
- *  - значение name не может быть короче чем 3 символа и длиннее чем 50 символов
- *  - name не может состоять из одних только пробелов
- *  - не зависимо от того ввел пользователь имя с большой или с маленькой буквы,
- *      оно должно быть записано в переменную name с большой буквы
- *  - ageGroup должен устанавливаться автоматически при установке возраста
- *      - child если age до 15 лет
- *      - student  - если age от 15 до 25 лет
- *      - worker - если age от 26 до 65 лет
- *      - pensioner - если age старше 66 лет
- *  - ageGroup можно только прочитать с помощью геттера, сеттер должен быть приватным и недоступным для других классов
- */
-
 public class Task1 {
 
-}
+    private int age;
+    private String name;
+    private String ageGroup;
+
+
+    public void setAge(int age) {
+        if (age <= 0 || age > 100) {
+            System.out.println("Please enter the valid age from 0 to 100");
+        } else {
+            this.age = age;
+        }
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setName(String name) {
+        if (name.isEmpty() || ( name.length() < 3 ) || name.trim().isEmpty() ) {
+            System.out.println("The name should contain more than 3 symbols");
+        } else if (name.length() > 3 && name.length() <= 49) {
+            System.out.println(name.substring(0, 1).toUpperCase() + name.substring(1));
+        } else if (name.length() > 50) {
+            System.out.println(name.substring(0, 1).toUpperCase() + name.substring(1,50));
+        }
+        System.out.println(this.name);
+
+
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public void setAgeGroup(){
+            this.ageGroup = ageGroup;
+            int age = getAge();
+            if (age <= 15) {
+                System.out.println("Child");
+            } else if (age > 15 && age <= 25) {
+                System.out.println("Student");
+            } else if (age > 25 && age <= 65) {
+                System.out.println("Worker");
+            } else if (age > 65 && age <= 100) {
+                System.out.println("Pensioner");
+            } else if (age > 100) {
+                System.out.println("Group is not defined");
+            }
+            System.out.println(this.ageGroup);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/L1task1.java
@@ -0,0 +1,30 @@
+import lesson1.Task1;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class L1task1 {
+    Task1 t1 = new Task1();
+
+
+
+
+
+    @Test(dataProvider = "SetVariables")
+    public void firstTest(int a, int b, int c, int expected){
+    t1.min(a,b,c);
+    int actual = t1.min(a,b,c);
+     assertEquals(expected, actual, "the minimum is 5");
+
+    }
+
+    @DataProvider(name = "SetVariables")
+    public Object[][] SetVariables(){
+        return new Object[][]{
+                {10, 5, 12, 5},
+                {10, 25, 100, 10},
+        };
+    }
+
+  }
