e0de341b964e40d3599189d26bcbcd9f7b54829d
Natalia <nataozira@gmail.com>
2020-02-12 20:05:35 +0200

    Home task Done

---
+++ src/test/java/L1Task5.java
@@ -0,0 +1,15 @@
+import lesson1.Task5;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class L1Task5 {
+    Task5 t5 = new Task5();
+    @Test(enabled=true)
+    public void thirdTest(){
+        String expected = "!!!dlrow olleH";
+        String actual = t5.reverse();
+         assertEquals(actual, expected);
+
+    }
+}
